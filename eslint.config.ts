eceb6cb73ae6879cd0b110c5aec6ec0687ad5163
Benedict Lee <benedictleejh@gmail.com>
2024-10-20 22:21:32 +0800

    Enable ESLint for Pug

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,7 +1,15 @@
+import { dirname } from 'node:path'
+import { fileURLToPath } from 'node:url'
+import { FlatCompat } from '@eslint/eslintrc'
 import withNuxt from './.nuxt/eslint.config.mjs'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
+const compat = new FlatCompat({ baseDirectory: __dirname })
 
 export default withNuxt(
   // Your custom configs here
+  ...compat.extends('plugin:vue-pug/vue3-recommended'),
   {
     rules: {
       '@stylistic/comma-dangle': ['error', 'never']
