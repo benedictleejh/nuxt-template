a8d35894a1cdcf9ddfc4e9538b78b320def39319
Benedict Lee <benedictleejh@gmail.com>
2024-10-26 18:20:23 +0800

    Disable parallel typechecking and linting in precommit
    Due to race conditions on reading package.json for typechecking

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,13 +1,11 @@
 /** @type { import('lint-staged').Config } */
 export default {
   '*.{ts,tsx,js,jsx,vue}': stagedFiles => [
-    `eslint --max-warnings 0 --flag unstable_ts_config ${stagedFiles.join(' ')}`
+    `pnpm lint:es ${stagedFiles.join(' ')}`,
+    'pnpm typecheck',
+    'pnpm test:unit'
   ],
   '*.{css,less,scss,sass,pcss,postcss,sss,vue}': stagedFiles => [
     `stylelint ${stagedFiles.join(' ')}`
-  ],
-  '*.*': () => [
-    'pnpm typecheck',
-    'pnpm test:unit'
   ]
 }
