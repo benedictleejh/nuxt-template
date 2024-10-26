c5bed3b7437b92a432e4fa487bfe2d8effa5239b
Benedict Lee <benedictleejh@gmail.com>
2024-10-26 19:13:59 +0800

    Parallelise linting, typechecking, and tests

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,11 +1,13 @@
 /** @type { import('lint-staged').Config } */
 export default {
   '*.{ts,tsx,js,jsx,vue}': stagedFiles => [
-    `pnpm lint:es ${stagedFiles.join(' ')}`,
-    'pnpm typecheck',
-    'pnpm test:unit'
+    `pnpm lint:es ${stagedFiles.join(' ')}`
   ],
   '*.{css,less,scss,sass,pcss,postcss,sss,vue}': stagedFiles => [
     `stylelint ${stagedFiles.join(' ')}`
+  ],
+  '*.*': () => [
+    'pnpm typecheck',
+    'pnpm test:unit'
   ]
 }
