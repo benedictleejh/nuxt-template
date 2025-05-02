a10a46df977c3bcb6ec291f34929df53c3fd7671
Benedict Lee <benedictleejh@gmail.com>
2025-05-02 18:12:16 +0800

    Use eslint-plugin-n to enforce usage of node protocol

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,9 +1,16 @@
 import { FlatCompat } from '@eslint/eslintrc'
+import n from 'eslint-plugin-n'
 import withNuxt from './.nuxt/eslint.config.mjs'
 
 const compat = new FlatCompat({ baseDirectory: import.meta.dirname })
 
 export default withNuxt(
   // Your custom configs here
-  ...compat.extends('plugin:vue-pug/vue3-recommended')
+  ...compat.extends('plugin:vue-pug/vue3-recommended'),
+  {
+    plugins: { n },
+    rules: {
+      'n/prefer-node-protocol': ['error']
+    }
+  }
 )
