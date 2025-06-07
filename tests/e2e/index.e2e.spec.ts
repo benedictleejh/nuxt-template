c77fa0fc0486270c71110cea5878350620134665
Benedict Lee <benedictleejh@gmail.com>
2025-06-07 20:43:47 +0800

    Fix e2e test

--- tests/e2e/index.e2e.spec.ts
+++ tests/e2e/index.e2e.spec.ts
@@ -2,6 +2,13 @@ import { expect, test } from '@nuxt/test-utils/playwright'
 
 test('Index page Welcome', async ({ page, goto }) => {
   await goto('/', { waitUntil: 'hydration' })
-  await expect(page.getByRole('heading', { level: 1 })).toHaveText('Welcome to Nuxt!')
-  await expect(page.getByRole('heading', { level: 2 })).toContainText('Get started')
+
+  await expect(page).toHaveTitle('Welcome to Nuxt!')
+  await expect(page.getByRole('heading', { level: 2 })).toHaveText([
+    'Get started',
+    'Documentation',
+    'Modules',
+    'Examples',
+    'Deploy'
+  ])
 })
