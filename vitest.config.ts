b28ba333e1e6dc381c5f2ae10ac2e6eb7d211730
Benedict Lee <benedictleejh@gmail.com>
2025-12-14 22:42:37 +0800

    Refactor workaround for using tsconfig aliases in non-Nuxt Vitest projects

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,74 +1,67 @@
-import path from 'node:path'
-
 import { defineVitestProject } from '@nuxt/test-utils/config'
 import { defineConfig as defineVitestConfig } from 'vitest/config'
 
+const vitestProjects = async () => {
+  // Workaround for https://github.com/nuxt/test-utils/issues/1408
+  const nuxtAppProject = await defineVitestProject({
+    test: {
+      name: 'app',
+      include: [
+        'tests/app/{components,composables,layouts,middleware,pages,plugins}/*.{test,spec}.ts'
+      ],
+      environment: 'nuxt',
+      environmentOptions: {
+        nuxt: {
+          domEnvironment: 'jsdom'
+        }
+      }
+    }
+  })
+
+  return [
+    {
+      // TODO: remove when Nuxt Test Utils provides a way to automatically add this to the project
+      resolve: nuxtAppProject.resolve,
+      test: {
+        name: 'shared',
+        include: [
+          'tests/shared/utils/*.{test,spec}.ts'
+        ],
+        environment: 'node'
+      }
+    },
+    {
+      // TODO: remove when Nuxt Test Utils provides a way to automatically add this to the project
+      resolve: nuxtAppProject.resolve,
+      test: {
+        name: 'app:utils',
+        include: [
+          'tests/app/utils/*.{test,spec}.ts'
+        ],
+        environment: 'node'
+      }
+    },
+    nuxtAppProject,
+    await defineVitestProject({
+      test: {
+        setupFiles: './.config/vitest.setup.ts',
+        name: 'server',
+        include: [
+          'tests/server/{api,middleware,plugins,routes,utils}/*.{test,spec}.ts'
+        ],
+        environment: 'nuxt',
+        environmentOptions: {
+          nuxt: {
+            domEnvironment: 'jsdom'
+          }
+        }
+      }
+    })
+  ]
+}
+
 export default defineVitestConfig({
   test: {
-    projects: [
-      {
-        // TODO: remove when Nuxt Test Utils provides a way to automatically add this to the project
-        resolve: {
-          alias: {
-            '~': path.resolve(import.meta.dirname, 'app'),
-            '~~': import.meta.dirname,
-            '#shared': path.resolve(import.meta.dirname, 'shared')
-          }
-        },
-        test: {
-          name: 'shared',
-          include: [
-            'tests/shared/utils/*.{test,spec}.ts'
-          ],
-          environment: 'node'
-        }
-      },
-      {
-        // TODO: remove when Nuxt Test Utils provides a way to automatically add this to the project
-        resolve: {
-          alias: {
-            '~': path.resolve(import.meta.dirname, 'app'),
-            '~~': import.meta.dirname,
-            '#shared': path.resolve(import.meta.dirname, 'shared')
-          }
-        },
-        test: {
-          name: 'app:utils',
-          include: [
-            'tests/app/utils/*.{test,spec}.ts'
-          ],
-          environment: 'node'
-        }
-      },
-      await defineVitestProject({
-        test: {
-          name: 'app',
-          include: [
-            'tests/app/{components,composables,layouts,middleware,pages,plugins}/*.{test,spec}.ts'
-          ],
-          environment: 'nuxt',
-          environmentOptions: {
-            nuxt: {
-              domEnvironment: 'jsdom'
-            }
-          }
-        }
-      }),
-      await defineVitestProject({
-        test: {
-          setupFiles: './.config/vitest.setup.ts',
-          name: 'server',
-          include: [
-            'tests/server/{api,middleware,plugins,routes,utils}/*.{test,spec}.ts'
-          ],
-          environment: 'nuxt',
-          environmentOptions: {
-            nuxt: {
-              domEnvironment: 'jsdom'
-            }
-          }
-        }
-      })
-    ]
+    projects: await vitestProjects()
   }
 })
